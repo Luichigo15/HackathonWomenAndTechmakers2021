653c60b42067e2d96ed55e8de421ea6bb0b848cf
Jose <jose.satoritech@gmail.com>
2021-11-26 22:16:05 -0600

    questions fragment

--- app/src/main/java/com/reyozic/hackathon/ui/view/HWTMainActivity.kt
+++ app/src/main/java/com/reyozic/hackathon/ui/view/HWTMainActivity.kt
@@ -7,12 +7,16 @@ import androidx.appcompat.app.AppCompatActivity
 import com.reyozic.hackathon.databinding.ActivityMainBinding
 import com.reyozic.hackathon.domain.constant.HWTActualFragment
 import com.reyozic.hackathon.ui.controls.fragment.HWTFragmentContainerHelper
+import com.reyozic.hackathon.ui.view.fragments.LoginFragment
+import com.reyozic.hackathon.ui.view.fragments.QuestionsFragment
 
-class HWTMainActivity : AppCompatActivity(){
+class HWTMainActivity : AppCompatActivity(), QuestionsFragment.Listener{
 
     private lateinit var binding: ActivityMainBinding
 
     private lateinit var fragmentContainer:HWTFragmentContainerHelper
+
+    private var questionsFragment: QuestionsFragment = QuestionsFragment.newInstance()
 
     private var actualFragment = HWTActualFragment.QUESTIONS
 
@@ -27,7 +31,9 @@ class HWTMainActivity : AppCompatActivity(){
     private fun getViewContainer(): View? = binding.content
 
     private fun initElements(){
+        questionsFragment.setListener(this)
         fragmentContainer = HWTFragmentContainerHelper(this,getViewContainer())
+        fragmentContainer.updateFragmentContainer(questionsFragment,true)
 
     }
 }
